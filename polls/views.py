38425120a072108a224c77f7acede50de6660fc3
Giga <asefhosseini00@gmail.com>
2024-02-20 12:46:47 +0330

    use not found request shortcut

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import Http404
 
 # Create your views here.
@@ -16,10 +16,12 @@ def index(request):
   return render(request, "polls/index.html", viewModel)
 
 def detail(request, question_id):
-  try:
-    question = Question.objects.get(pk=question_id)
-  except Question.DoesNotExist:
-    raise Http404("Question does not exist")
+  question = get_object_or_404(Question, pk=question_id)
+
+  # try:
+  #   question = Question.objects.get(pk=question_id)
+  # except Question.DoesNotExist:
+  #   raise Http404("Question does not exist")
   return render(request, "polls/details.html", {"question": question})
 
 def results(request, question_id):
